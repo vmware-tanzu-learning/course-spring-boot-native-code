95b055fcb94277fab5d737c73816fe1b468980ef
Sébastien Deleuze <sdeleuze@vmware.com>
2023-07-13 17:11:19 +0200

    <initial> Use template rendering
    To illustrate the need for reflection and resource
    hints for native.

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,11 +1,11 @@
 package example.cashcard;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+public interface CashCardRepository extends ListCrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
     CashCard findByIdAndOwner(Long id, String owner);
 
     boolean existsByIdAndOwner(Long id, String owner);
