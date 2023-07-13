bc6b84544ec18bbd297386031df15679f872b59c
Sébastien Deleuze <sdeleuze@vmware.com>
2023-07-13 22:35:32 +0200

    Add resource hint

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -1,13 +1,25 @@
 package example.cashcard;
 
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(CashCardApplication.Hints.class)
 public class CashCardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CashCardApplication.class, args);
+	}
+
+	static class Hints implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+			hints.resources().registerPattern("cashcard-banner.txt");
+		}
 	}
 
 }
